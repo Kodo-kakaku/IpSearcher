14882669e57d61d5b5868b8d64b74b34c94e62c1
unknown <spanky@yandex.ru>
2021-03-31 10:42:15 +0300

    Revert "remove all files for review"
    This reverts commit 4c873eed2bac925c3b6b53659b1902328bf9d245.

---
+++ README.md
@@ -0,0 +1,3 @@
+# IpSearcher [![Build Status](https://travis-ci.org/Kodo-kakaku/IpSearcher.svg?branch=main)](https://travis-ci.org/Kodo-kakaku/IpSearcher)
+
+I hate templates and metaprogramming they make me cry!
---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "../lib/printus.h"
+
+int main() {
+
+    print_ip(static_cast<char>(-1));
+    std::cout << "\n";
+
+    print_ip(static_cast<short>(0));
+    std::cout << "\n";
+
+    print_ip(static_cast<int>(2130706433));
+    std::cout << "\n";
+
+    print_ip(static_cast<long long>(8875824491850138409));
+    std::cout << "\n";
+
+    print_ip(std::string("Hello, World!"));
+    std::cout << "\n";
+
+    const std::vector<int> v{ 100, 200, 300, 400 };
+    print_ip(std::vector<int>(v));
+    std::cout << "\n";
+
+    const std::list<short> l{ 400, 300, 200, 100 };
+    print_ip(std::list<short>(l));
+    std::cout << "\n";
+
+    print_ip(std::make_tuple(123, 456, 789, 0));
+    std::cout << std::endl;
+
+    return 0;
+}
